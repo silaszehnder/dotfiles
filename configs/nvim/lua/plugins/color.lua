5f0405333bf28bf5ccf9638bdeb968d31121fbe6
Silas Zehnder <silaszehnder@gmail.com>
2025-06-04 14:08:13 -0400

    feat: nvim intro

---
+++ configs/nvim/lua/plugins/color.lua
@@ -0,0 +1,15 @@
+return {
+-- Using Lazy
+{
+  "navarasu/onedark.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('onedark').setup {
+      style = 'dark',
+      transparent = true
+    }
+    -- Enable theme
+    require('onedark').load()
+  end
+},
+}
